b4e660b121d0aeadd5b0c1fe1be92528e894be44
AymoJB <aymojobe@gmail.com>
2024-12-28 16:13:59 +0100

    webpack setup

---
+++ src/index.js
@@ -0,0 +1,5 @@
+console.log('hello world from index.js ');
+
+import { changebodycontent } from './API.js';
+
+changebodycontent('HELLO WORLD FROM API.js');
--- webpack.config.js
+++ webpack.config.js
@@ -1,5 +1,48 @@
-
-
-// module.exports = {
-//     entry: 
-// }
+const path = require('path')
+module.exports = {
+    entry: './src/index.js',
+    output: {
+        path: path.resolve(__dirname, './dist'),
+        filename: "bundle.js"
+    },
+    mode: "development",
+    watch:true,
+    optimization: {
+        splitChunks: {
+          chunks: "all",
+        },
+      },
+    module: {
+        rules: [
+            {
+                test: /\.css$/,
+                use: [
+                    'style-loader',
+                    'css-loader'
+                ]
+            },
+            {
+                test: /\.(png|svg|jpg|gif)$/,
+                use: ["file-loader"],
+            },
+            {
+                test: /\.js$/,
+                exclude: /node_modules/,
+                use: {
+                  loader: "babel-loader",
+                  options: {
+                    presets: ["@babel/preset-env"],
+                  },
+                },
+              },
+        ]
+    },
+    devServer: {
+        static: {
+            directory: path.resolve(__dirname, 'dist'),
+        },
+        devMiddleware: {
+            publicPath: '/assets/'
+        }
+    }
+}
